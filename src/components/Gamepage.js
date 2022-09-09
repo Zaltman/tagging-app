63f7805ad0e490cc7b0434e6da5aa70f27e24a7c
Marius <mariusdruskinis@gmail.com>
2022-09-09 17:50:47 +0300

    Css improvement

--- src/components/Gamepage.js
+++ src/components/Gamepage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function GamePage() {
   return (
     <div className="flex items-center justify-center">
-      <div className="font-bold rounded-lg m-36 text-center">
+      <div className="font-bold rounded-lg m-16 text-center">
         <h2 className="font-bold text-xl rounded-lg text-center mb-5">Choose level</h2>
         <Link to={'/level1'} className="text-center w-full">
           Level 1
