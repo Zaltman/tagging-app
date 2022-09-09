e5369de6db313059854106aab99969a4f5e5f76f
Marius <mariusdruskinis@gmail.com>
2022-09-09 17:41:10 +0300

    Css improvement

--- src/components/Gamepage.js
+++ src/components/Gamepage.js
@@ -2,9 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function GamePage() {
   return (
-    <div className="gamePage">
-      <p>Choose level</p>
-      <Link to={'/level1'}>Level 1</Link>
+    <div className="flex items-center justify-center">
+      <div className="font-bold rounded-lg m-36 text-center">
+        <h2 className="font-bold text-xl rounded-lg text-center mb-5">Choose level</h2>
+        <Link to={'/level1'} className="text-center w-full">
+          Level 1
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,12 +2,14 @@
 // import Register from './Register';
 export default function Homepage() {
   return (
-    <div className="homePage">
-      <h1>Main info</h1>
+    <div className="flex items-center justify-center">
+      <div className="font-bold rounded-lg m-36">
+        <h1>Main info</h1>
 
-      <h2>Lorem ipsum dolor sit amet.</h2>
-      <h2>Id esse neque maxime itaque.</h2>
-      <h2>Animi dolores maiores culpa laudantium!</h2>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <h2>Id esse neque maxime itaque.</h2>
+        <h2>Animi dolores maiores culpa laudantium!</h2>
+      </div>
     </div>
   );
 }
